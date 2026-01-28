92aeb8825f84dec11eafab96f73ccc73864d1174
Techbitforge <techbitforgeoffice.com@gmail.com>
2026-01-28 20:11:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -594,5 +594,6 @@ def signin_page():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 1000))
+    port = int(os.environ.get("PORT", 7860))
     app.run(host="0.0.0.0", port=port,debug=False)
+
